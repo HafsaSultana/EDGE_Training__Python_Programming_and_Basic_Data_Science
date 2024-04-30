89f4817bf377bf3611bf62bdf55aac468cc541a2
HafsaSultana <hafsa1720@cseku.ac.bd>
2024-05-01 00:48:15 +0600

    Intoduction of Python Basic

---
+++ Day2_BasicPython.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+print(5+5)
+5+5
+
+print("####____________#####")
+
+for i in range(5):
+    print("Number ",i)
+
+print("####____________#####")
+    
+import math 
+y = math.sqrt(25)
+print(y)
+
+print("####____________#####")
+
+a=5
+b=10
+greater = a > b
+print(greater)
+
+print("####____________#####")
+
+x=10
+
+if x>0:
+    print("Positive")
+elif x==0:
+    print("Zero")
+else:
+    print("Negative")
+    
+print("####____________#####")
+
+def greet(name):
+    print("Hello ", name, "!")
+    x=4
+    x = 4+3
+    return x
+greet("Hafsa")
+
+print("####____________#####")
+
+x="Python"
+y="is"
+z="Awesome"
+print(x + y + z)
+print(x,y,z)
+
+print("####____________#####")
+
+x=2.4
+print(int(x))
+
+
